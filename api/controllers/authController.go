9db247b0834fbbd83de8341df1fb1e11209cf655
matsukazu0918 <kazu.baske74@gmail.com>
2021-11-04 16:36:14 +0900

    ログイン、ログアウト処理、トークン生成、パスワードリセットテーブル作成

--- api/controllers/authController.go
+++ api/controllers/authController.go
@@ -3,10 +3,41 @@ package controllers
 import (
 	"auth-api/database"
 	"auth-api/models"
+	"strconv"
+	"time"
 
+	"github.com/dgrijalva/jwt-go"
 	"github.com/gofiber/fiber/v2"
 	"golang.org/x/crypto/bcrypt"
 )
+
+type Claims struct {
+	jwt.StandardClaims
+}
+
+func User(c *fiber.Ctx) error {
+	// CookieからJWTを取得
+	cookie := c.Cookies("jwt") // Loginで保存したもの
+	// token取得
+	token, err := jwt.ParseWithClaims(cookie, &Claims{}, func(token *jwt.Token) (interface{}, error) {
+		return []byte("secret"), nil
+	})
+	if err != nil || !token.Valid {
+		c.Status(fiber.StatusUnauthorized)
+		return c.JSON(fiber.Map{
+			"message": "unauthenticated",
+		})
+	}
+
+	claims := token.Claims.(*Claims)
+	// User IDを取得
+	id := claims.Issuer
+
+	var user models.User
+	database.DB.Where("id = ?", id).First(&user)
+
+	return c.JSON(user)
+}
 
 func Register(c *fiber.Ctx) error {
 	var data map[string]string
@@ -37,4 +68,67 @@ func Register(c *fiber.Ctx) error {
 	database.DB.Create(&user)
 
 	return c.JSON(user)
+}
+
+// ログイン
+func Login(c *fiber.Ctx) error {
+	var data map[string]string
+
+	if err := c.BodyParser(&data); err != nil {
+		return err
+	}
+
+	var user models.User
+	database.DB.Where("email = ?", data["email"]).First(&user)
+	if user.ID == 0 {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "User not found",
+		})
+	}
+	// パスワードのチェック
+	if err := bcrypt.CompareHashAndPassword(user.Password, []byte(data["password"])); err != nil {
+		c.Status(404)
+		return c.JSON(fiber.Map{
+			"message": "Incorrect password",
+		})
+	}
+
+	// JWT
+	claims := jwt.StandardClaims{
+		Issuer:    strconv.Itoa(int(user.ID)),            // stringに型変換
+		ExpiresAt: time.Now().Add(time.Hour * 24).Unix(), // 有効期限
+	}
+	jwtToken := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+	token, err := jwtToken.SignedString([]byte("secret"))
+	if err != nil {
+		return c.SendStatus(fiber.StatusInternalServerError)
+	}
+
+	// Cookie
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    token,
+		Expires:  time.Now().Add(time.Hour * 24),
+		HTTPOnly: true,
+	}
+	c.Cookie(&cookie)
+
+	return c.JSON(fiber.Map{
+		"jwt": token,
+	})
+}
+
+func Logout(c *fiber.Ctx) error {
+	cookie := fiber.Cookie{
+		Name:     "jwt",
+		Value:    "",                         // tokenを空にする
+		Expires:  time.Now().Add(-time.Hour), // マイナス値を入れて期限切れ
+		HTTPOnly: true,
+	}
+
+	c.Cookie(&cookie)
+	return c.JSON(fiber.Map{
+		"message": "success",
+	})
 }
